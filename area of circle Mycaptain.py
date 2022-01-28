acc2ea639997e8228a213c4d15f8d13b4c76e427
varshaa-vj <98607983+varshaa-vj@users.noreply.github.com>
2022-01-28 10:45:02 -0800

    Add files via upload

---
+++ area of circle Mycaptain.py
@@ -0,0 +1,9 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#1 circle
+radius=int(input("enter a number:"))
+c=(2*3.14*radius)
+a=(3.14*radius*radius)
+print("the circumference of circle is",c,"the area of the circle is",a)
+enter a number:3
+
